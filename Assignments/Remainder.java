3d6d905a441f5c236f3a06e11f355ec7cc9b2004
Riyabiraris2005 <birarisriya1@gmail.com>
2024-08-23 22:15:10 +0530

    Update Remainder.java

--- Assignments/Remainder.java
+++ Assignments/Remainder.java
@@ -1,1 +1,11 @@
+package student;
 
+public class remainder{
+    public static void main(String[] args) {
+        int dividend = 10;
+        int divisor = 3;
+        int quotient = dividend / divisor;
+        int remainder = dividend - (quotient * divisor);
+        System.out.println("The remainder is: " + remainder);
+    }
+}
